e2e12dde032369906e4d3d3dd355520d4c9a7c00
Samuel Toledo <samueltoledo.bmanagement@outlook.com>
2024-05-09 21:20:53 -0600

    Actualizado hasta primeros eventos

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // MAIN JS PAISES APP
+
+document.querySelector(".clear-icon").addEventListener("click", () => {
+    const searchValue = document.querySelector(".search-input");
+    searchValue.value = "";
+    searchValue.focus();
+    console.log(searchValue.value);
+});
+
+document.querySelector(".search-country").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const country = document.querySelector(".search-input").value;
+    console.log(country);
+})
